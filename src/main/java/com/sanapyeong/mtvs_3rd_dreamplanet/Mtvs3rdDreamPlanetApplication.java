02a38d19cda4a890594762254e808ab1b18e9628
daekwon2000 <daekklove@gmail.com>
2024-10-21 16:52:24 +0900

    first commit for initializing

---
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/Mtvs3rdDreamPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.sanapyeong.mtvs_3rd_dreamplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mtvs3rdDreamPlanetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mtvs3rdDreamPlanetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanapyeong/mtvs_3rd_dreamplanet/Mtvs3rdDreamPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanapyeong.mtvs_3rd_dreamplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mtvs3rdDreamPlanetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
