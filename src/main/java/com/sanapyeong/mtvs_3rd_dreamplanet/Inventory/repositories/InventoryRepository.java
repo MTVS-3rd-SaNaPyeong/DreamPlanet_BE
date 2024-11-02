5a0c6bb3117b60a5457a89026d46a643eedc485e
daekwon2000 <daekklove@gmail.com>
2024-11-02 17:28:57 +0900

    [Feat] 유저의 블록 행성 인벤토리 반환 구문 작성

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/Inventory/repositories/InventoryRepository.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/Inventory/repositories/InventoryRepository.java
@@ -1,6 +1,7 @@
 package com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.repositories;
 
 
+import com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.dto.BlockInventoryFindResponseDTO;
 import com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.entities.Inventory;
 import com.sanapyeong.mtvs_3rd_dreamplanet.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,10 +11,9 @@ import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    //List<Inventory> findBlockInventoryByUserID(Long userId);
+    @Query("SELECT i.playedPlanetId, i.postedLocation, b.completedWork " +
+            "FROM Inventory i LEFT JOIN PlayedBlockPlanet b on i.playedPlanetId = b.id " +
+            "WHERE i.userId = :userId AND i.planetType = 'BLOCK' AND i.storedStatus = true")
+    List<Object[]> findBlockInventoryByUserId(Long userId);
 
-
-
-    @Query("SELECT u FROM User u WHERE u.loginId = :loginId")
-    User findUserByLoginId(String loginId);
 }
