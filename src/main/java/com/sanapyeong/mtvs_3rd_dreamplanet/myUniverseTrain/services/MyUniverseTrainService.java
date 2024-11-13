86cec6deb155c93b081a194bec1e5c4f3c4bc4a1
daekwon2000 <daekklove@gmail.com>
2024-11-14 01:12:57 +0900

    [Feat] My Universe Train 검색 기능 추가 - trainName 또는 uniqueCode로 검색

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/myUniverseTrain/services/MyUniverseTrainService.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/myUniverseTrain/services/MyUniverseTrainService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.dto.BlockInventoryFindResponseDTO;
 import com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.repositories.InventoryRepository;
 import com.sanapyeong.mtvs_3rd_dreamplanet.myUniverseTrain.dto.MyUniverseTrainFindResponseDTO;
+import com.sanapyeong.mtvs_3rd_dreamplanet.myUniverseTrain.dto.MyUniverseTrainSummaryFindResponseDTO;
 import com.sanapyeong.mtvs_3rd_dreamplanet.myUniverseTrain.entities.MyUniverseTrain;
 import com.sanapyeong.mtvs_3rd_dreamplanet.myUniverseTrain.repositories.MyUniverseTrainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class MyUniverseTrainService {
@@ -171,5 +174,17 @@ public class MyUniverseTrainService {
         );
 
         return findResult;
+    }
+
+    public List<MyUniverseTrainSummaryFindResponseDTO> findMyUniverseTrainsBySearchWord(String searchWord) {
+
+        List<MyUniverseTrainSummaryFindResponseDTO> foundList = null;
+
+        foundList = myUniverseTrainRepository.findMyUniverseTrainsBySearchWord(searchWord)
+                .stream()
+                .map(MyUniverseTrainSummaryFindResponseDTO::new)
+                .toList();
+
+        return foundList;
     }
 }
