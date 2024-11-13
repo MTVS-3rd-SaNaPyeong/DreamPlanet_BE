02deb8a9cdb81c9bca11982aa81c24c4923b49fc
daekwon2000 <daekklove@gmail.com>
2024-11-14 01:12:55 +0900

    [Feat] My Universe Train 검색 기능 추가 - trainName 또는 uniqueCode로 검색

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/myUniverseTrain/repositories/MyUniverseTrainRepository.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/myUniverseTrain/repositories/MyUniverseTrainRepository.java
@@ -6,10 +6,18 @@ import com.sanapyeong.mtvs_3rd_dreamplanet.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface MyUniverseTrainRepository extends JpaRepository<MyUniverseTrain, Long> {
 
     @Query("SELECT m FROM MyUniverseTrain m WHERE m.userId = :userId")
     MyUniverseTrain findMyUniverseTrainByUserId(Long userId);
+
+    @Query("SELECT m.id, m.userId, m.trainName, m.uniqueCode " +
+            "FROM MyUniverseTrain m " +
+            "WHERE m.trainName like %:searchWord% OR m.uniqueCode like %:searchWord%")
+    List<Object[]> findMyUniverseTrainsBySearchWord(String searchWord);
 
 //    @Query("SELECT u FROM User u WHERE u.loginId = :loginId")
 //    User findUserByLoginId(String loginId);
