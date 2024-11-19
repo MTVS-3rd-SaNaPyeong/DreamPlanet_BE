150ee217fe348c22d3c567908cec54447b3ba71c
daekwon2000 <daekklove@gmail.com>
2024-11-19 17:43:40 +0900

    [Fix] Inventory repository 수정

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/Inventory/repositories/InventoryRepository.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/Inventory/repositories/InventoryRepository.java
@@ -14,9 +14,13 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     // PlanetType BLOCK_TYPE = PlanetType.BLOCK;
 
-    @Query("SELECT i.id, i.playedPlanetId, i.postedLocation, b.completedWork " +
-            "FROM Inventory i LEFT JOIN PlayedBlockPlanet b on i.playedPlanetId = b.id " +
-            "WHERE i.userId = :userId AND i.planetType = com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.enums.PlanetType.BLOCK AND i.storedStatus = true")
-    List<Object[]> findBlockInventoryByUserId(Long userId);
+    @Query("SELECT i.id, i.playedPlanetId, p.postedLocation, b.completedWork " +
+            "FROM Inventory i " +
+                "LEFT JOIN PlayedBlockPlanet b on i.playedPlanetId = b.id " +
+                "LEFT JOIN PostingInfo p on i.id = p.inventoryId AND p.myUniverseTrainId = :myUniverseTrainId " +
+            "WHERE i.userId = :userId " +
+                "AND i.planetType = com.sanapyeong.mtvs_3rd_dreamplanet.Inventory.enums.PlanetType.BLOCK " +
+                "AND i.storedStatus = true")
+    List<Object[]> findBlockInventoryByUserIdAndMyUniverseTrainId(Long userId, Long myUniverseTrainId);
 
 }
