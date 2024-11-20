d69c72dfd4a21df4a2f713a291d1168fb44bdaa4
daekwon2000 <daekklove@gmail.com>
2024-11-20 21:34:40 +0900

    [Feat] TempBlockPlanetStorage Repository 생성

---
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/playedBlockPlanet/repositories/TempBlockPlanetStorageRepository.java
@@ -0,0 +1,9 @@
+package com.sanapyeong.mtvs_3rd_dreamplanet.playedBlockPlanet.repositories;
+
+import com.sanapyeong.mtvs_3rd_dreamplanet.playedBlockPlanet.entities.TempBlockPlanetStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TempBlockPlanetStorageRepository extends JpaRepository<TempBlockPlanetStorage, Long> {
+
+
+}
