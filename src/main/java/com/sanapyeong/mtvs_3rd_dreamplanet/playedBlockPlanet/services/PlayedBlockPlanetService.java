e3cd586f08f458ab903a05cf6bb6c9027db9326d
daekwon2000 <daekklove@gmail.com>
2024-11-21 15:39:22 +0900

    [Fix] Played Block Planet Service 수정

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/playedBlockPlanet/services/PlayedBlockPlanetService.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/playedBlockPlanet/services/PlayedBlockPlanetService.java
@@ -52,6 +52,25 @@ public class PlayedBlockPlanetService {
     }
 
     @Transactional
+    public void saveIdx(Long playedBlockPlanetId, Long idx) {
+        Optional<PlayedBlockPlanet> optionalPlayedBlockPlanet = playedBlockPlanetRepository.findById(playedBlockPlanetId);
+
+        // 엔티티가 존재하는지 확인
+        if (optionalPlayedBlockPlanet.isPresent()) {
+            PlayedBlockPlanet playedBlockPlanet = optionalPlayedBlockPlanet.get();
+
+            // completed work 정보 업데이트
+            if (idx != null) {
+                playedBlockPlanet.setIdx(idx);
+            }
+
+            // 자동으로 변경된 내용이 영속성 컨텍스트에 반영됨 (Transactional 덕분)
+        } else {
+            throw new IllegalArgumentException("ID에 해당하는 블록 행성이 존재하지 않습니다.");
+        }
+    }
+
+    @Transactional
     public void saveColorDotImage(Long playedBlockPlanetId, String storedURL){
         Optional<PlayedBlockPlanet> optionalPlayedBlockPlanet = playedBlockPlanetRepository.findById(playedBlockPlanetId);
 
