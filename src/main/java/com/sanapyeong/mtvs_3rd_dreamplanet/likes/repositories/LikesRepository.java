e9e869848ed9aa29d1fff08f7a8e482f74daafd4
daekwon2000 <daekklove@gmail.com>
2024-12-11 18:33:57 +0900

    [Feat] LikesRepository - PostingInfoId를 통한 Likes 내역 찾기 쿼리문 추가

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/likes/repositories/LikesRepository.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/likes/repositories/LikesRepository.java
@@ -4,6 +4,7 @@ import com.sanapyeong.mtvs_3rd_dreamplanet.likes.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
@@ -12,4 +13,9 @@ public interface LikesRepository extends JpaRepository<Likes, Long> {
             "FROM Likes l " +
             "WHERE l.userId = :userId AND l.postingInfoId = :postingInfoId")
     Optional<Likes> findByUserIdAndPostingInfoId(Long userId, Long postingInfoId);
+
+    @Query("SELECT l " +
+            "FROM Likes l " +
+            "WHERE l.postingInfoId = :postingInfoId")
+    List<Likes> findByPostingInfoId(Long postingInfoId);
 }
