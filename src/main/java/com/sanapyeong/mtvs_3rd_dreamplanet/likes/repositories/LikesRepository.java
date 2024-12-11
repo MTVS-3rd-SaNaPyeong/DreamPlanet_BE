daa0d831e9dfde405890d4975de0029231696e45
daekwon2000 <daekklove@gmail.com>
2024-12-11 18:14:09 +0900

    [Feat] LikesRepository - 좋아요 내역 검색 쿼리문 추가

--- src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/likes/repositories/LikesRepository.java
+++ src/main/java/com/sanapyeong/mtvs_3rd_dreamplanet/likes/repositories/LikesRepository.java
@@ -2,6 +2,14 @@ package com.sanapyeong.mtvs_3rd_dreamplanet.likes.repositories;
 
 import com.sanapyeong.mtvs_3rd_dreamplanet.likes.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
+
+    @Query("SELECT l " +
+            "FROM Likes l " +
+            "WHERE l.userId = :userId AND l.postingInfoId = :postingInfoId")
+    Optional<Likes> findByUserIdAndPostingInfoId(Long userId, Long postingInfoId);
 }
